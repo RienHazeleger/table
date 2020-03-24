f7e576542f9d7ef2c44226a268dc3ddf9e843699
Rien Hazeleger <rien.hazeleger@gmail.com>
2020-03-24 14:09:04 +0000

    Created js files and included styling for h2 and ul

---
+++ js/my_script.js
@@ -0,0 +1,4 @@
+$(document).ready((function) {
+       $("h2").css("text-decoration", "underline"); //underlines all h2 elements
+       $("ul").css("border", "1px solid #ccc"); //gives the unordered list a border of 1px solid, 
+});
